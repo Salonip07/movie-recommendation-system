2490ffa5aa481bd3f2095b85fc53212d7b2e1ab3
Saloni Pabreja <salonipabreja06@gmail.com>
2026-01-15 23:04:35 +0530

    Refactor UserProfile class and add new methods

--- backend/user_profile.py
+++ backend/user_profile.py
@@ -1,23 +1,24 @@
 class UserProfile:
     def __init__(self):
         self.watch_history = {}
-        self.watch_count = {}
-        self.genre_hours = {}
         self.wishlist = set()
-        self.ratings = {}
-        self.bucket = {}
+        self.day_bucket = []
+        self.night_bucket = []
 
-    def watch_movie(self, movie_id, watch_time):
-        self.watch_history[movie_id] = watch_time
-        self.watch_count[movie_id] = self.watch_count.get(movie_id, 0) + 1
-        if self.watch_count[movie_id] >= 3:
-            self.bucket[movie_id] = self.watch_count[movie_id]
+    def add_watch(self, movie, hours, pref):
+        self.watch_history[movie] = self.watch_history.get(movie, 0) + hours
+        if pref == "Day":
+            self.day_bucket.append(movie)
+        else:
+            self.night_bucket.append(movie)
 
-    def rate_movie(self, movie_id, rating):
-        self.ratings[movie_id] = rating
+    def favorite_genres(self):
+        return ["Sci-Fi", "Action"]
 
-    def add_to_wishlist(self, movie_id):
-        self.wishlist.add(movie_id)
-
-    def get_bucket(self):
-        return self.bucket
+    def export(self):
+        return {
+            "watch_history": self.watch_history,
+            "wishlist": list(self.wishlist),
+            "day_bucket": self.day_bucket,
+            "night_bucket": self.night_bucket
+        }
